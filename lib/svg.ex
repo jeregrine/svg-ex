57c2840f5bb15f1a48865799a5d1b2a13794bd12
Jason Stiebs <jason@fly.io>
2023-10-11 10:47:19 -0500

    make it so

---
+++ lib/svg.ex
@@ -0,0 +1,18 @@
+defmodule SVG do
+  @moduledoc """
+  Documentation for `SVG`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> SVG.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/svg_test.exs
@@ -0,0 +1,8 @@
+defmodule SVGTest do
+  use ExUnit.Case
+  doctest SVG
+
+  test "greets the world" do
+    assert SVG.hello() == :world
+  end
+end
